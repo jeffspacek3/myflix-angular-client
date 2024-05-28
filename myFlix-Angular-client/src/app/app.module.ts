bcf4790cd8915525d69c151b42e3da1034fcf61b
Jeff Spacek <Jeffrey.spacek@gmail.com>
2024-05-28 07:40:14 -0700

    installed '@angular/material' . Added 'app.module.ts' and imports

--- myFlix-Angular-client/src/app/app.module.ts
+++ myFlix-Angular-client/src/app/app.module.ts
@@ -2,14 +2,39 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { FormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { LoginUserFormComponent } from './login-user-form/login-user-form.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [AppComponent, UserRegistrationFormComponent, LoginUserFormComponent],
-  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    UserRegistrationFormComponent,
+    LoginUserFormComponent,
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatDialogModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatSnackBarModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
