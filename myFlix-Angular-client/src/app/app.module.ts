a72336fa10d29d6784e5882aaa4c8e1e4f6c7e0f
Jeff Spacek <Jeffrey.spacek@gmail.com>
2024-05-27 20:30:01 -0700

    update user-registration compnents. install new component 'login-user-form'

--- myFlix-Angular-client/src/app/app.module.ts
+++ myFlix-Angular-client/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { LoginUserFormComponent } from './login-user-form/login-user-form.component';
 
 @NgModule({
-  declarations: [AppComponent, UserRegistrationFormComponent],
+  declarations: [AppComponent, UserRegistrationFormComponent, LoginUserFormComponent],
   imports: [BrowserModule, HttpClientModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ myFlix-Angular-client/src/app/login-user-form/login-user-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login-user-form',
+  templateUrl: './login-user-form.component.html',
+  styleUrls: ['./login-user-form.component.scss']
+})
+export class LoginUserFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
